ae053ba030d85790fa1f09c9ce48510fc44d19a4
Zaal Vasania <zaalvasania@gmail.com>
2020-06-10 22:03:10 +0800

    Interpreter completely edited for infinite loop detection and error checking

--- brainFk.py
+++ brainFk.py
@@ -4,24 +4,11 @@ class BrainFuckInterpreter:
         self.pointer = 0
 
     def interpret(self,inputData):
-        self.list, self.pointer, l, i = [0], 0, 0, -1
-        pattern, instrCheck, countReps = "", "",0
+        self.list, self.pointer, l, i, j = [0], 0, 0, -1, 0
         output = ""
+        if(inputData.count("[") != inputData.count("]")): return(None, False)
         while (i<len(inputData)-1):
             i+=1
-            print("Pattern:",pattern)
-            print("Input at Index:",inputData[i])
-            print("Input index:",i)
-            if(pattern!=""):
-                instrCheck+=inputData[i]
-                if(pattern == instrCheck):
-                    instrCheck = ""
-                    countReps+=1
-                else:
-                    if(len(pattern) == len(instrCheck)):
-                        print(instrCheck)
-                        patternCheck, instrCheck, countReps = "", "", 0
-            if(countReps>10): return(None,False)
             if (inputData[i]=="<"):
                 if(self.pointer>=1):
                     self.pointer-=1
@@ -32,6 +19,7 @@ class BrainFuckInterpreter:
                 self.pointer+=1
             elif (inputData[i]=="+"):
                 self.list[self.pointer]+=1
+                # Change to mod 127 for it to interpret properly
                 self.list[self.pointer]%=95
             elif(inputData[i]=="-"):
                 if(self.list[self.pointer]>0):
@@ -40,7 +28,8 @@ class BrainFuckInterpreter:
                     return (None,False) # Non interpretable expression
             elif(inputData[i]=="."):
                 try:
-                    output+=chr(self.list[self.pointer] + 33)
+                    # Take away +33
+                    output+=chr(self.list[self.pointer] + 33) 
                 except ValueError: return(None, False)
             elif(inputData[i]=="["):
                 if(self.list[self.pointer]==0):
@@ -54,13 +43,10 @@ class BrainFuckInterpreter:
                         return (None,False) # Non interpretable expression
             elif(inputData[i]=="]"):
                 if(self.list[self.pointer]>0):
-                    print("Input Data:",inputData)
                     i-=1
                     if(i>=0):
                         collate = 0
-                        boolPattCheck = True if(pattern=="") else False
                         while(l>0 or inputData[i]!="["):
-                            if(boolPattCheck): pattern+=inputData[i+1]
                             if(i==0):
                                 return (None, False) # Non interpretable expression
                             if(inputData[i]=="]"): l+=1
@@ -68,15 +54,11 @@ class BrainFuckInterpreter:
                             if (inputData[i] in ["+","-",">","<"]): collate +=1
                             i-=1
                         if (collate==0): return(None, False) # Non interpretable expression
-                        if(boolPattCheck):
-                            pattern+=inputData[i+1]
-                            pattern = "".join(reversed(pattern))
                     else: return(None,False)
-                else: pattern, instrCheck, countReps = "", "", 0
+            j+=1
+            # Can't detect infinite loop due to Halting Problem 
+            if(j>4000): return(None,False)
         if(output==""): return(None,False)
-        print(output)
-        print(inputData)
-        print("reached")
         return (output,True)
 
 if __name__ == "__main__":
